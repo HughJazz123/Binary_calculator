00193d2b7141c9f60348da4f26430c5bfed47e22
HughJazz123 <50455574+HughJazz123@users.noreply.github.com>
2020-04-18 12:49:56 +0800

    Update binary_addition_calculator_v2.py

--- binary_addition_calculator_v2.py
+++ binary_addition_calculator_v2.py
@@ -11,12 +11,6 @@ def AND(a,b):
     else:
         return 0
 
-def OR(a,b):
-    if a==1 or b==1:
-        return 1
-    else:
-        return 0
-
 def XOR(a,b):
     if a==1 or b==1:
         if a==1 and b==1:
